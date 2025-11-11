bf406ec9805f112ab0abfe20a679f3d37bea3fa4
Paul Vincent Javier <polbinsenthabyer@gmail.com>
2025-11-11 19:16:17 +0800

    Initial commit

---
+++ src/main/java/com/project/eloms/ElomsApplication.java
@@ -0,0 +1,13 @@
+package com.project.eloms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eloms/ElomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eloms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
